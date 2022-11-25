4762ca1aae7ae4ce07700b889b516f13e4f22b0d
IlyaKonovaloB <103986019+IlyaKonovaloB@users.noreply.github.com>
2022-11-25 11:22:34 +0300

    Add files via upload

---
+++ dashboard(25.11)/index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const server = http.createServer(app);
+const port = 3000;
+const dashboard = require('./server/routes/api/dashboard');
+const jsonfile = require('jsonfile');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.disable("x-powered-by");
+
+let data = require('./data.json');
+let script = require('./views/script.js');
+app.use((err, req, res, next) => {
+	logger.error(err.stack);
+	res.status(500).send("Вы сломали сервер!");
+});
+
+app.use((err, req, res, next) => {
+	if (error instanceof ForbiddenError) {
+		return res.status(403).send({
+			status: "forbidden",
+			message: error.message,
+		});
+	}
+});
+
+app.put('/edit/:id', function(req, res) {
+    var id = req.params.id;
+    var newText = req.body.text;
+    jsonfile.readFile('data.json', function(err, obj) {
+      var fileObj = obj;
+      fileObj[id].text = newText;
+      jsonfile.writeFile('data.json', fileObj, function(err) {
+          if (err) throw err;
+      });
+    });
+	res.json('ok')
+});
+app.post('/addTask', (req, res) => {
+	if (!req.body) return res.sendStatus(400)
+	const user = {
+	  id: file.length,
+	  text: req.body.text,
+	}
+	jsonfile.readFile('data.json', (err, obj) => {
+	  if (err) throw err
+	  let fileObj = obj;
+	  fileObj.push(user);
+	  jsonfile.writeFile('data.json', fileObj, (err) => {
+		if (err) throw err;
+	  })
+	  res.send(obj)
+	})
+  })
+  
+  app.delete('/delete/:id', (req, res) => {
+	jsonfile.readFile('data.json', (err, obj) => {
+	  if (err) throw err
+	  let fileObj = obj;
+	  for(let i = 0; i < fileObj.length; i++) {
+		if (fileObj[i].id == req.params.id) {
+		  fileObj.splice(i, 1)
+		}
+	  }
+	  jsonfile.writeFile('data.json', fileObj, { spaces: 2 }, (err) => {
+		if (err) throw err;
+	  })
+	  res.send(obj)
+	})
+  })
+app.set('view engine', 'pug')
+
+app.use(express.static("views"));
+
+
+app.use('/dashboard', function (request, response) {
+	response.render('index',{data}),
+	response.render('index',{script})})
+
+
+	app.use('/registr', function (request, response) {
+response.render('registr',)
+})
+// Routes
+// app.use("/registr", regi);
+
+
+app.use(express.static("client/build"));
+// самому поменять
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./", "client", "build", "index.html"));
+	// самому поменять
+});
+
+//Go the SERVERs
+server.listen(port, () => {
+	console.log("\x1b[35m%s\x1b[0m", `The server is running on the port ${port}`);
+	console.log("\x1b[32m%s\x1b[0m", `http://localhost:${port}/`);
+	// console.log(`Worker ${cluster.worker.id} launched`);
+});
